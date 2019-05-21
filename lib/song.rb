ece6e2a86bda64855ce797ad34f3821a9496767e
Michael Chen <michaelchengaming@gmail.com>
2019-05-21 13:12:45 -0700

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+
+class Song
+  
+  @@all = []
+  
+  attr_accessor :name, :artist 
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artist_name
+    artist.nil? ? nil : artist.name
+  end
+
+end
