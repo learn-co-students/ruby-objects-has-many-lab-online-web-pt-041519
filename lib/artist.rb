44bccef82df68d769f30e38d66b0c115a4c32c43
github username <github email address>
2019-05-21 22:47:36 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+class Artist
+  attr_accessor :name , :songs
+
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+
+   def add_song(song)
+     self.songs << song
+     song.artist = self
+    end
+
+    def add_song_by_name(name)
+      song = Song.new(name)
+      self.songs << song
+      song.artist = self
+    end
+
+    def self.song_count
+      Song.all.count
+    end
+
+end
