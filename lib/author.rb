ac0c4bf12cfdb56932b4a8b15aa6e077b4b95ef8
Cody Frank <cody.frank30@gmail.com>
2019-05-25 00:12:13 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,25 @@
+class Author
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def self.post_count
+    Post.all.length
+  end
+
+  def posts
+    @posts
+  end
+
+  def add_post(post)
+    self.posts << post
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    add_post(post)
+  end
+end
